f2cd638af1b0f2fc4d285c66e51877aea28fdfcb
michael <micfall.romtin@gmail.com>
2016-12-20 04:32:36 +0900

    feat: implement main logic

--- apps/jamesbot/src/jamesbot_srv.erl
+++ apps/jamesbot/src/jamesbot_srv.erl
@@ -11,7 +11,7 @@
 %% Application callbacks
 -export([start_link/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, code_change/3, terminate/2]).
-
+-export([post/1, delete/1]).
 -record(state, {handler, text, id_str, sent, deleted}).
 
 %%====================================================================
@@ -25,6 +25,7 @@ start_link(_) ->
   erorr.
 
 init({HandlerPid, Text}) ->
+  self() ! send_tweet,
   {ok, #state{handler = HandlerPid, text = Text, sent = false, deleted = false} }.
 
 %%--------------------------------------------------------------------
@@ -42,15 +43,26 @@ handle_call(_Request, _From, State) ->
 handle_cast(_Info, State) ->
   {noreply, State}.
 
-handle_info(Op, State) ->
-
-  {noreply, State}.
+handle_info(_Op, State = #state{handler = HandlerPid, text = Text, id_str = Id, sent = Sflag, deleted = Dflag}) ->
+  case {Sflag, Dflag} of
+    {false, _} ->
+      Res = post(Text ++ binary_to_list(?SUFFIX)),
+      NewId = find_id(Res),
+      self() ! delete_tweet,
+      {noreply, State#state{id_str = NewId, sent = true}};
+    {true, false} ->
+      Res = timer:apply_after(?INTERVAL * 1000, jamesbot_srv, delete, [Id]),
+      _ = parse_response(Res),
+      {noreply, State#state{deleted = true} };
+    {true, true} ->
+      {stop, normal, #state{}}
+  end.
 
 code_change(_Old, State, _Extra) ->
   {ok, State}.
 
 %%====================================================================
-%% Internal functions
+%% public functions
 %%====================================================================
 
 post(Text) ->
@@ -64,6 +76,10 @@ delete(TweetIdStr) ->
   ApiKey = {?CK, ?CS, hmac_sha1},
   URL = "https://api.twitter.com/1.1/statuses/destroy/" ++ TweetIdStr ++ ".json",
   Response = oauth:post(URL, [{"id", TweetIdStr}], ApiKey, ?AT, ?AS).
+
+%%====================================================================
+%% private functions
+%%====================================================================
 
 find_id(Response) ->
   BodyJson = jsx:decode(list_to_binary(parse_response(Response))),
